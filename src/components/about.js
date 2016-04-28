c30d0ee99a799265a86fc14a5da149bedb4fb35f
Dino <dinotrojak@gmail.com>
2016-04-28 17:14:52 +0200

    added about

--- src/components/about.js
+++ src/components/about.js
@@ -1,21 +1,48 @@
 import '../styles/about.styl'
 import React from 'react';
 import Title from './title'
-import headerImg from '../images/header_img_about.jpg';
+import img1 from '../images/about.png';
+import img2 from '../images/about2.png';
+import img3 from '../images/about3.png';
 
 class Schedule extends React.Component {
   render() {
     return (
       <div className="about">
-        <Title title="About" bck={headerImg}/>
+        <Title title="About"/>
         <div className="container group">
-          <section>
-            <div className="titleTxt">
-              Glavni organizator festivala je Udruge MOP dok je organizator festivala multimedije je Turistička zajednica grada Varaždina (TZGV).
+          <div className="titleTxt">
+            <h2>MOP ŠPANCIRFEST
+                is a multimedia festival.
+            </h2>
+            <p>Program festivala obuhvaća predavanja, tjedne radionice, izložbe, video projekcije, interaktivne instalacije, performanse, filmske projekcije te audio-vizualne instalacije integrirane u njegov glazbeni dio. Od ove godine partner Udruge MOP i suorganizator festivala multimedije je Turistička zajednica grada Varaždina (TZGV).</p>
+          </div>
+          <img src={img1}/>
+          <div className="section">
+            <div className="item">
+              <h3>History</h3>
             </div>
-            <div className="cell">
-              Program Festivala najviše je povezan s multimedijom i digitalnom kulturom, pri čemu se nastoji aktivirati što veći broj studenata, predavača, multimedijalnih umjetnika, glazbenika i ostalih kreativaca kako bi se međusobno razmijenila znanja i povećali potencijali. Program Festivala najviše je povezan s multimedijom i digitalnom kulturom, pri čemu se nastoji aktivirati što veći broj studenata, predavača, multimedijalnih umjetnika, glazbenika i ostalih kreativaca kako bi se međusobno razmijenila znanja i povećali potencijali.             </div>
-          </section>
+            <div className="item">
+              <p>Festival je do danas održan 4 puta kao godišnji regionalni festival kroz koji se promiče multimedija i digitalna kultura s naglaskom na razmjenu znanja i kreativno korištenje novih tehnologija u umjetnosti, znanosti i dizajnu.</p>
+              <div className="past">
+                <h4>Past editions</h4>
+                <a>12</a>
+                <a>13</a>
+                <a>14</a>
+                <a>15</a>
+              </div>
+            </div>
+          </div>
+          <img src={img2}/>
+          <div className="section">
+            <div className="item">
+              <h3>Location</h3>
+            </div>
+            <div className="item">
+              <p>Festival je do danas održan 4 puta kao godišnji regionalni festival kroz koji se promiče multimedija i digitalna kultura s naglaskom na razmjenu znanja i kreativno korištenje novih tehnologija u umjetnosti, znanosti i dizajnu.</p>
+            </div>
+          </div>
+          <img src={img3}/>
         </div>
       </div>
     );
